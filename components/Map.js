e92f43901ac52acb149b1e13f95f5939b7d93cc0
EchlasMalik <EchlasMalik1@gmail.com>
2025-02-09 02:33:11 +0000

    Implemented maps with mapRef and resizing

--- components/Map.js
+++ components/Map.js
@@ -1,15 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import tw from 'tailwind-react-native-classnames';
 import { useSelector } from 'react-redux';
-import { selectOrigin } from '../slices/navSlice';
+import { selectOrigin, selectDestination } from '../slices/navSlice';
+import MapViewDirections from 'react-native-maps-directions';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const Map = () => {
     const origin = useSelector(selectOrigin);
+    const destination = useSelector(selectDestination);
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+      if (!origin || !destination) return;
+
+      // Zoom & fit to markers
+      setTimeout(() => {
+        mapRef.current.fitToSuppliedMarkers(["origin", "destination"], {
+          edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
+        });
+      }, 500);
+    }, [origin, destination])
 
   return (
-    <MapView 
+    <MapView
+        ref={mapRef}
         style ={tw`flex-1`}
         mapType='mutedStandard'
         initialRegion={{
@@ -19,6 +35,16 @@ const Map = () => {
             longitudeDelta: 0.005,
         }}
     >
+      {origin && destination && (
+        <MapViewDirections
+          origin={origin.description}
+          destination={destination.description}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="black"
+        />
+      ) }
+
       {origin?.location && (
         <Marker
             coordinate={{
@@ -28,6 +54,18 @@ const Map = () => {
             title="Origin"
             description={origin.description}
             identifier="origin"
+        />
+      )}
+
+      {destination?.location && (
+        <Marker
+            coordinate={{
+              latitude: destination.location.lat,
+              longitude: destination.location.lng,
+            }}
+            title="Destination"
+            description={destination.description}
+            identifier="destination"
         />
       )}
     </MapView>
--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -5,15 +5,24 @@ import tw from 'tailwind-react-native-classnames'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { useDispatch } from 'react-redux'
 import { setDestination } from '../slices/navSlice'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native';
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
     const dispatch = useDispatch();
     const navigation = useNavigation();
+    const currentHour = new Date().getHours();
+    let greeting = "";
+
+    if (currentHour < 12 ){greeting = "Good Morning!"}
+    else {
+        greeting = "Good Evening!"
+    }
+
 
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Good Morning</Text>
+      <Text style={tw`text-center py-5 text-xl -mt-10`}>{greeting}</Text>
         <View style={tw`border-t border-gray-200 flex-shrink`}>
             <View>
                 <GooglePlacesAutocomplete
